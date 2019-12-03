28a0b85c87c23a0eb6ad3d08060be5cc7b869948
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2019-12-03 15:37:04 +0100

    bring setup.py up to snuff

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
-
+import os
 from setuptools import setup
+
+
+version = os.environ.get("CIRCLE_TAG") or "0.0.1-dev"
+url = "https://github.com/dreamdata-io/tap-googlesearch"
 
 setup(
     name="tap-googlesearch",
-    version="0.0.1",
-    description="Singer.io tap for extracting data AgileCMS",
+    version=version,
+    description="Singer.io tap for extracting data from Google Search Analytics",
     author="Dreamdata",
-    url="https://dreamdata.io",
+    url=url,
+    download_url=f"{url}/archive/v{version}.tar.gz",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
+    license="MIT",
     install_requires=[
-        "singer-python==5.8.1",
+        "singer-python>=5.8.1, <6",
         "google-api-python-client",
         "ratelimit",
         "backoff",
